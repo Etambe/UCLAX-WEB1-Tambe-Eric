db1f428ae084563b58c75a3eec491e3eec283ae5
Eric Tambe <etambe1@gmail.com>
2025-10-16 10:28:13 -0700

    Revert "added layout and pages components"
    This reverts commit 9811e553da04cdf395c5f9580ea91e5146d85183.

--- src/frontend/App/index.jsx
+++ src/frontend/App/index.jsx
@@ -1,18 +1,44 @@
-import { BrowserRouter } from "react-router-dom";
+import { useState } from "react";
+import styled from "styled-components";
 
-// components
-import AppRoutes from "./Routes";
-import Layout from "./Layout";
+const SunAndMoonStyled = styled.div`
+    text-align: center;
 
-// component
-const App = () => {
+    h2 {
+        font-size: 30px;
+        color: #784a00;
+    }
+
+    img {
+        display: block;
+        margin: 30px auto;
+        padding: 20px;
+        border: solid 2px teal;
+        background-color: #014b4b;
+    }
+`;
+
+const SunAndMoon = () => {
+    const [srcState, setSrcState] = useState("/assets/sunAndMoon/moon.png");
+
+    const changeToSun = () => {
+        setSrcState("/assets/sunAndMoon/sun.png");
+    };
+    const changeToMoon = () => {
+        setSrcState("/assets/sunAndMoon/moon.png");
+    };
+
     return (
-        <BrowserRouter>
-            <Layout>
-                <AppRoutes />
-            </Layout>
-        </BrowserRouter>
+        <SunAndMoonStyled>
+            <h2>Sun and Moon</h2>
+            <img
+                src={srcState}
+                alt="Sun and Moon"
+                onMouseEnter={changeToSun}
+                onMouseLeave={changeToMoon}
+            />
+        </SunAndMoonStyled>
     );
 };
 
-export default App;
+export default SunAndMoon;
