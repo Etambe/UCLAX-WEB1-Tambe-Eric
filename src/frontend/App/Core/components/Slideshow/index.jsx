bd1a0535670bf1751635e4d9c23158b38453955b
Mitch Gohman <mitch@thenounproject.com>
2025-12-03 17:56:36 -0700

    MG Rev: Carousel showThumbs={false}
    This suppresses the warning about not using thumbnails for navigation.

--- src/frontend/App/Core/components/Slideshow/index.jsx
+++ src/frontend/App/Core/components/Slideshow/index.jsx
@@ -8,7 +8,7 @@ import Slide from "./Slide";
 // component
 const Slideshow = ({ slides }) => {
     return (
-        <Carousel>
+        <Carousel showThumbs={false}>
             {slides.map((slide) => {
                 return <Slide key={slide.id} slide={slide} />;
             })}
